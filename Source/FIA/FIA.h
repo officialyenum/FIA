f448768ed3b3a3f48159db50fae4715dedaf3bd5
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2026-08-06 00:52:00 +0100

    add debug macros
    Add on-screen debug macros in Source/FIA/FIA.h (FIA_LOG, FIA_WARN, FIA_ERROR) with keyed and formatted variants to standardize engine logging.

--- Source/FIA/FIA.h
+++ Source/FIA/FIA.h
@@ -4,11 +4,18 @@
 
 #include "CoreMinimal.h"
 
-//Logs
+//LOG
 #define FIA_LOG(Text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Black, TEXT(Text), false)
 #define FIA_LOG_K(Key, Text) if (GEngine) GEngine->AddOnScreenDebugMessage(Key, 2.0f, FColor::Black, TEXT(Text), false)
 #define FIA_LOG_F(Format, ...) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Black, FString::Printf(TEXT(Format), ##__VA_ARGS__), false)
 #define FIA_LOG_F_K(Key, Format, ...) if (GEngine) GEngine->AddOnScreenDebugMessage(Key, 2.0f, FColor::Black, FString::Printf(TEXT(Format). ##__VA_ARGS__)
+
+//WARN
+#define FIA_WARN(Text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Orange, TEXT(Text), false)
+#define FIA_WARN_K(Key, Text) if (GEngine) GEngine->AddOnScreenDebugMessage(Key, 2.0f, FColor::Orange, TEXT(Text), false)
+#define FIA_WARN_F(Format, ...) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Orange, FString::Printf(TEXT(Format), ##__VA_ARGS__), false)
+#define FIA_WARN_F_K(Key, Format, ...) if (GEngine) GEngine->AddOnScreenDebugMessage(Key, 2.0f, FColor::Orange, FString::Printf(TEXT(Format). ##__VA_ARGS__)
+
 //ERRORS
 #define FIA_ERROR(Text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT(Text), false)
 #define FIA_ERROR_K(Key, Text) if (GEngine) GEngine->AddOnScreenDebugMessage(Key, 2.0f, FColor::Red, TEXT(Text), false)
